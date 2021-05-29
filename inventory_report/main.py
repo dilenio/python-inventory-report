0cf9b49367d3e88b2e13931121e6c066db53796b
Dilenio Enderle <dilenio@gmail.com>
2021-05-29 18:10:49 -0300

    feat: req 6 and 7 iterator

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,21 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
     pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+    else:
+        _, file_path, report_type = sys.argv
+        if file_path.endswith('.csv'):
+            instance = InventoryRefactor(CsvImporter)
+        if file_path.endswith('.json'):
+            instance = InventoryRefactor(JsonImporter)
+        if file_path.endswith('.xml'):
+            instance = InventoryRefactor(XmlImporter)
+        report = instance.import_data(file_path, report_type)
+        print(report, end="")
